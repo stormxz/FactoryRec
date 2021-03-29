3ceb2dc069ad6a73fccc9af5ebb14205f8aeafd9
stormxz <15151860920@163.com>
2021-03-29 19:57:14 +0800

    init factoryrec code
    Signed-off-by: stormxz <15151860920@163.com>

---
+++ app/src/main/java/com/example/factoryrec/Fragment_OM.java
@@ -0,0 +1,18 @@
+package com.example.factoryrec;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Fragment_OM extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_menu_om, container, false);
+    }
+}
