356d26fbb5895a78aaf2db5fee106a2d199a1a0f
stormxz <15151860920@163.com>
2021-05-24 14:40:24 +0800

    add ftp function patch 2
    Signed-off-by: stormxz <15151860920@163.com>

--- app/src/main/java/com/example/factoryrec/app/Fragment_Upload.java
+++ app/src/main/java/com/example/factoryrec/app/Fragment_Upload.java
@@ -1,5 +1,6 @@
 package com.example.factoryrec.app;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -104,9 +105,9 @@ public class Fragment_Upload extends MainFragment implements View.OnClickListene
         // TODO 可以首先去判断一下网络
         FTPClientFunctions ftpClient = new FTPClientFunctions();
 
-        boolean connectResult = ftpClient.ftpConnect("ftp.arcsoft.com.cn", "CoolPad_DLNA", "weC5uq9G", 21);
+//        boolean connectResult = ftpClient.ftpConnect("ftp.arcsoft.com.cn", "CoolPad_DLNA", "weC5uq9G", 21);
         // 连接FTP
-//        boolean connectResult = ftpClient.ftpConnect(hostname, username, password, 21);
+        boolean connectResult = ftpClient.ftpConnect(hostname, username, password, 21);
 
         if (connectResult) {
             Log.i(TAGUPLOAD, "connect ftp success");
@@ -129,6 +130,8 @@ public class Fragment_Upload extends MainFragment implements View.OnClickListene
                     File mNewFile = new File(fileNameDir.getAbsolutePath() + File.separator + "/" + submitTimer +"/");
                     File[] tempList = mNewFile.listFiles();
                     boolean uploadResult = false;
+                    setUploadButEnable(false);
+                    showShowToast("正在进行文件上传");
                     for (int i = 0; i < tempList.length; i++) {
                         if (tempList[i].isFile()) {
                             // 上传文件
@@ -148,22 +151,24 @@ public class Fragment_Upload extends MainFragment implements View.OnClickListene
                         // 断开连接
                         boolean disConnectResult = ftpClient.ftpDisconnect();
                         if (disConnectResult) {
+                            setUploadButEnable(true);
+                            showShowToast("文件上传成功");
                             Log.e(TAGUPLOAD, "disConnectResult sucess");
                         } else {
                             Log.e(TAGUPLOAD, "disConnectResult fail");
-                            shotErrorToast("断开FTP 连接失败");
+                            showShowToast("断开FTP 连接失败");
                         }
                     } else {
                         Log.i(TAGUPLOAD, "uploadResult fail");
-                        shotErrorToast("上传失败，请查看网络是否稳定");
+                        showShowToast("上传失败，请查看网络是否稳定");
                     }
                 } else {
                     Log.i(TAGUPLOAD, "ftpChangeDir fail");
-                    shotErrorToast("检查目录失败，请查看网络是否稳定");
+                    showShowToast("检查目录失败，请查看网络是否稳定");
                 }
             } else {
                 Log.i(TAGUPLOAD, "connect ftp fail");
-                shotErrorToast("创建存储目录失败");
+                showShowToast("创建存储目录失败");
             }
 
 
@@ -171,20 +176,38 @@ public class Fragment_Upload extends MainFragment implements View.OnClickListene
 
         } else {
             Log.i(TAGUPLOAD, "connect ftp fail");
-            shotErrorToast("登录失败，请检查FTP 输入内容是否正确");
+            showShowToast("登录失败，请检查FTP 输入内容是否正确");
         }
     }
 
-    private void shotErrorToast(final String errorMessage) {
+    private void showShowToast(final String message) {
 
         mActivity.runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                Toast.makeText(getContext(), errorMessage, Toast.LENGTH_SHORT).show();
+                Context context = getContext();
+                if (context == null) return;
+                Toast.makeText(getContext(), message, Toast.LENGTH_SHORT).show();
             }
         });
     }
 
+    private void setUploadButEnable(final boolean isEnable) {
+        mActivity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if (mUploadBut != null) {
+                    if (!isEnable) {
+                        mUploadBut.setText("上传中...");
+                    } else {
+                        mUploadBut.setText("上传");
+                    }
+                    mUploadBut.setEnabled(isEnable);
+                }
+            }
+        });
+
+    }
 
     private String getTime() {
         Calendar calendar = Calendar.getInstance();
